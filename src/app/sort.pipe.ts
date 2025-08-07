bba15f1af9756b6f0ffcbe02721f6074bd4fc37b
Nizaam Yunus <nizaamcoleyunus18@gmail.com>
2025-08-07 20:38:09 +0200

    Added logic to the pipe

--- src/app/sort.pipe.ts
+++ src/app/sort.pipe.ts
@@ -6,8 +6,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SortPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string[] | number[], direction: 'asc' | 'desc' = 'asc') {
+    const sorted = [...value];
+    sorted.sort((a, b) => {
+      if (direction === 'asc') {
+        return a > b ? 1 : -1;
+      } else {
+        return a > b ? -1 : 1;
+      }
+    });
+    return sorted;
   }
 
 }
